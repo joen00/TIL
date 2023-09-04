e718cbaab484dad808c4a4060bdf1c3c6347ba8e
김아영 <joen00@naver.com>
2023-09-04 11:52:53 +0900

    알고리즘
    알고리즘

---
+++ algorithm/August/src/week5/PRO_큰수만들기.java
@@ -0,0 +1,24 @@
+package week5;
+
+public class PRO_큰수만들기 {
+
+	public String solution(String number, int k) {
+
+		StringBuilder answer = new StringBuilder("");
+		int len = number.length() - k;
+		int start = 0;
+		while (start < number.length() && answer.length() != len) {
+			int max = 0;
+			for (int j = start; j < k + answer.length() + 1; j++) {
+				if (max < number.charAt(j) - '0') {
+					max = number.charAt(j) - '0';
+					start = j + 1;
+				}
+			}
+			answer.append(Integer.toString(max));
+		}
+
+		return answer.toString();
+	}
+
+}
